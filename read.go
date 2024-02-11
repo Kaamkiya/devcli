64cf90c4b17f44df4fdfaa69a6d411a1feff12aa
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2024-02-11 16:24:58 -0500

    feat: Render article's Markdown to terminal
    Using github.com/MichaelMure/go-term-markdown, render the article
    that the user requested to STDOUT.

--- read.go
+++ read.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	md "github.com/MichaelMure/go-term-markdown"
 )
 
 func readArticle(articleName string) {
@@ -19,8 +21,12 @@ func readArticle(articleName string) {
 	for scanner.Scan() {
 		articleApi += scanner.Text() + "\n"
 	}
-	article := make(map[string]any)
-	api := []byte(articleApi)
-	json.Unmarshal(api, &article)
-	fmt.Println(article["body_markdown"])
+	article := make(map[string]interface{})
+	json.Unmarshal([]byte(articleApi), &article)
+
+	title := article["title"].(string)
+	body := article["body_markdown"].(string)	
+	output := md.Render(string(body), 80, 6)
+	fmt.Println("\033[1m" + title + "\033[0m")
+	fmt.Println(string(output))
 }
