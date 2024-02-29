528b7b19de326df3407f3c97e75da049336c7edd
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2024-02-29 16:20:14 -0500

    feat: Use urfave/cli/v2 for command line arguments

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.21.6
 require jaytaylor.com/html2text v0.0.0-20230321000545-74c2419ad056
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 /*
@@ -20,23 +22,86 @@ For example:
 	Then it would call the displayUser function, defined in user.go
 */
 func main() {
-	if len(os.Args) < 2 {
+	/*if len(os.Args) < 2 {
 		fmt.Println("Usage: devcli <command> [<args>] [<options>]")
 		os.Exit(-1)
+	}*/
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name: "show-comments",
+				Aliases: "c",
+				Usage: "show comments on an article",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "read",
+				Usage: "read an article. (devcli read <author>/<article_slug>)",
+				Action: func(*cli.Context) {
+					readArticle()
+				},
+			},
+			{
+				Name:  "my-articles",
+				Usage: "print a list of all of your articles",
+				Action: func(*cli.Context) {
+					myArticles()
+				},
+			},
+			{
+				Name: "write",
+				Usage: "write an article - NOT YET WORKING",
+				Action: func(*cli.Context) {
+					writeArticle()
+				},
+			},
+			{
+				Name: "latest",
+				Aliases: []string{"recent", "new"},
+				Usage: "fetch the 30 most recently posted articles on dev",
+				Action: func(*cli.Context) {
+					recentlyPosted()
+				},
+			},
+			{
+				Name: "following-tags",
+				Usage: "fetch all of the tags that you follow",
+				Action: func(*cli.Context) {
+					followingTags()
+				},
+			},
+			{
+				Name: "followers",
+				Usage: "print a list of your followers",
+				Action: func(*cli.Context) {
+					followers()
+				},
+			},
+			{
+				Name: "user",
+				Usage: "display information about a user",
+				Action: func(*cli.Context) {
+					// TODO: allow the user to pick the username
+					displayUser("kaamkiya")
+				},
+			},
+			{
+				Name: "readinglist",
+				Usage: "print your reading list",
+				Action: func(*cli.Context) {
+					readingList()
+				},
+			},
+		},
 	}
 
-	switch os.Args[1] {
-	case "read":
-		// this subcommand requires an argument
-		if len(os.Args) < 3 {
-			// if that required argument is not provided, exit with a help message
-			fmt.Println(`Usage: 
-  devcli read <author>/<article_slug> [<options>]
-  devcli read <article_id> [<options>]`)
-			os.Exit(-1)
-		}
-		// otherwise:
-		readArticle(os.Args[2])
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+
+	/*switch os.Args[1] {
 	case "my-articles":
 		// fetch the user's articles based on $DEV_API_KEY
 		myArticles()
@@ -83,5 +148,5 @@ Commands:
   my-articles             print a list of all of your articles`)
 	default:
 		fmt.Println("Not a valid command. Run \033[38;5;245mdevcli help\033[0m for help.")
-	}
+	}*/
 }
