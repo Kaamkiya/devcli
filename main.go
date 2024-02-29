2de41023fbb2b07286b4f279da2cadee4f32cc94
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2024-02-29 18:09:45 -0500

    chore: Remove dead comments
    When migrating to urfave/cli, I commented out code instead
    of deleting it just in case. This commit deleted those
    commented-out code blocks.
    
    - Dead comments

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v2"
@@ -22,16 +21,11 @@ For example:
 	Then it would call the displayUser function, defined in user.go
 */
 func main() {
-	/*if len(os.Args) < 2 {
-		fmt.Println("Usage: devcli <command> [<args>] [<options>]")
-		os.Exit(-1)
-	}*/
-
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name: "show-comments",
-				Aliases: "c",
+				Aliases: []string{"c"},
 				Usage: "show comments on an article",
 			},
 		},
@@ -39,59 +33,68 @@ func main() {
 			{
 				Name: "read",
 				Usage: "read an article. (devcli read <author>/<article_slug>)",
-				Action: func(*cli.Context) {
-					readArticle()
+				Action: func(*cli.Context) error {
+					// TODO: allow the user to pick the article
+					readArticle("kaamkiya/devcli-a-cli-for-devto-4a85")
+					return nil
 				},
 			},
 			{
 				Name:  "my-articles",
 				Usage: "print a list of all of your articles",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					myArticles()
+					return nil
 				},
 			},
 			{
 				Name: "write",
 				Usage: "write an article - NOT YET WORKING",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					writeArticle()
+					return nil
 				},
 			},
 			{
 				Name: "latest",
 				Aliases: []string{"recent", "new"},
 				Usage: "fetch the 30 most recently posted articles on dev",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					recentlyPosted()
+					return nil
 				},
 			},
 			{
 				Name: "following-tags",
 				Usage: "fetch all of the tags that you follow",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					followingTags()
+					return nil
 				},
 			},
 			{
 				Name: "followers",
 				Usage: "print a list of your followers",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					followers()
+					return nil
 				},
 			},
 			{
 				Name: "user",
 				Usage: "display information about a user",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					// TODO: allow the user to pick the username
 					displayUser("kaamkiya")
+					return nil
 				},
 			},
 			{
 				Name: "readinglist",
 				Usage: "print your reading list",
-				Action: func(*cli.Context) {
+				Action: func(*cli.Context) error {
 					readingList()
+					return nil
 				},
 			},
 		},
@@ -100,53 +103,5 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
-
-	/*switch os.Args[1] {
-	case "my-articles":
-		// fetch the user's articles based on $DEV_API_KEY
-		myArticles()
-	case "write":
-		// FIXME: this does not currently work
-		writeArticle()
-	case "latest":
-	case "recent":
-		// fetch the 30 most recent articles
-		recentlyPosted()
-	case "following-tags":
-		// fetch the tags a user follows
-		followingTags()
-	case "followers":
-		// fetch a user's follower list
-		followers()
-	case "user":
-		// this command requires an argument, so...
-		if len(os.Args) < 3 {
-			// ... if the argument is not given, exit with a help message
-			fmt.Println(`Usage: devcli user <username>`)
-			os.Exit(-1)
-		}
-		// otherwise, display the user
-		displayUser(os.Args[2])
-	case "readinglist":
-		// fetch a user's reading list. Requires $DEV_API_KEY
-		readingList()
-	case "help":
-		// help message
-		fmt.Println(`devcli - a CLI for dev.to
-
-Options:
-  -sc, --show-comments    with read subcommand, print the comments an article recieved
-
-Commands:
-  read                    print an article so that you can read it
-  user                    show information about a user
-  following-tags          show all of the tags that you follow
-  followers               write a list of all of your followers
-  readinglist             print your reading list and the command to use to read the article
-  latest                  print the IDs of the 30 most recently written articles
-  recent                  same as latest
-  my-articles             print a list of all of your articles`)
-	default:
-		fmt.Println("Not a valid command. Run \033[38;5;245mdevcli help\033[0m for help.")
-	}*/
 }
+
